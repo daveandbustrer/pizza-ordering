7c17e66fb10bc8feccc8bf789299b619a4a46f74
daveandbustrer <praneelcatchu@gmail.com>
2026-05-12 22:22:29 -0400

    added the start of the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
-import { Col, Container, Row, ToastContainer } from "react-bootstrap";
+import {
+  Col,
+  Container,
+  Navbar,
+  NavbarBrand,
+  Row,
+  ToastContainer,
+} from "react-bootstrap";
 import "./App.sass";
 import { Confirm } from "./componets/Confirm";
 import PizzaCard from "./componets/PizzaCard";
@@ -31,22 +38,24 @@ function App() {
 
   return (
     <>
+      <div className="z-3 position-fixed">
+        <Navbar className="bg-body-primary">
+          <Container>
+            <NavbarBrand>
+              <a href="#">Atomic Pizza</a>
+            </NavbarBrand>
+          </Container>
+        </Navbar>
+      </div>
       <div>
         <Container>
-          <Row
-            xs={"auto"}
-            sm={"auto"}
-            md={"auto"}
-            lg={"auto"}
-            xl={"auto"}
-            xxl={"auto"}
-          >
+          <Row xs={"auto"} sm={"auto"} md={"auto"} lg={"auto"}>
             <img src="atomic pizza colored logo.png" alt="atomic pizza logo" />
           </Row>
         </Container>
       </div>
       <ToastContainer position="top-start" className="p-3">
-        <div className="z-3 position-fixed">
+        <div className="z-3 position-fixed pt-5">
           {ordered.map(
             (item, index) =>
               item.visible && (
@@ -70,16 +79,7 @@ function App() {
       <Container className="z-1 position-absolute pt-5 start-50 translate-middle-x w-100">
         <Row className="justify-content-evenly">
           {pizzas.map((data) => (
-            <Col
-              xs={12}
-              sm={6}
-              md={4}
-              lg={3}
-              xl={2}
-              xxl={1}
-              className="mb-5"
-              key={data.id}
-            >
+            <Col xs={12} sm={6} md={4} lg={3} className="mb-5" key={data.id}>
               <PizzaCard data={data} setOrdered={display_confirm} />
             </Col>
           ))}
