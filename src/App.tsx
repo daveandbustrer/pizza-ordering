a68a5d4528f42f173a932b905bdc7b550f30865a
daveandbustrer <praneelcatchu@gmail.com>
2026-05-15 22:05:56 -0400

    made lazy load and sespence

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.sass";
 import HomePage from "./componets/HomePage";
-import MenuPage from "./componets/MenuPage";
 import MyNavBar from "./componets/MyNavBar";
+
+const MenuPage = lazy(() => import("./componets/MenuPage"));
 
 function App() {
   return (
     <>
       <MyNavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/menu" element={<MenuPage />} />
-        <Route path="/cart" element={""} />
-      </Routes>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/cart" element={""} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/componets/HomePage.tsx
+++ src/componets/HomePage.tsx
@@ -2,24 +2,37 @@ import { Col, Container, Row } from "react-bootstrap";
 function HomePage() {
   return (
     <>
-      <Container>
-        <Row className="justify-content-center">
-          <Col className="align-item-center">
-            <img
-              src="atomic pizza colored logo.png"
-              alt="atomic pizza logo"
-              className="img-fluid"
-            />
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row className="justify-content-center">
-          <Col>
-            <h1>Atomic Pizza website</h1>
-          </Col>
-        </Row>
-      </Container>
+      <header>
+        <Container>
+          <Row className="justify-content-center">
+            <Col className="align-item-center">
+              <img
+                src="atomic pizza colored logo.png"
+                alt="atomic pizza logo"
+                className="img-fluid"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+      <main>
+        <Container>
+          <Row>
+            <Col className="text-center">
+              <h1 className="display-1">
+                <u>
+                  <strong>Atomic Pizza website</strong>
+                </u>
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h5>this is an example text</h5>
+            </Col>
+          </Row>
+        </Container>
+      </main>
     </>
   );
 }
